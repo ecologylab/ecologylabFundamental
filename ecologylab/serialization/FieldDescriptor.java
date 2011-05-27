8172d202c71c09cf0e8014ebe17ea7be2a2c2c2e
quyin <quyin@ecologylab.net>
2011-05-27 21:30:28 +0000

    enable List in ElementState subclasses, using ArrayList as the default implementation on deserialization.

--- ecologylab/serialization/FieldDescriptor.java
+++ ecologylab/serialization/FieldDescriptor.java
@@ -29,9 +29,7 @@ import ecologylab.generic.ReflectionTools;
 import ecologylab.serialization.TranslationScope.GRAPH_SWITCH;
 import ecologylab.serialization.library.html.Anchor;
 import ecologylab.serialization.library.html.Div;
-import ecologylab.serialization.library.html.Span;
 import ecologylab.serialization.library.html.Td;
-import ecologylab.serialization.library.html.Tr;
 import ecologylab.serialization.types.scalar.MappingConstants;
 import ecologylab.serialization.types.scalar.ScalarType;
 import ecologylab.serialization.types.scalar.TypeRegistry;
@@ -1792,6 +1790,16 @@ public class FieldDescriptor extends ElementState implements FieldTypes
 				Class collectionType = field.getType();
 				try
 				{
+					// this is a workaround for enabling List in ElementState subclasses,
+					// it uses ArrayList as the default List implementation for List fields.
+					// we need List in ORM layer.
+					// clients can still use ArrayList, HashMapArrayList or any other List implementations
+					// if they need to.
+					// this could be improved, e.g. by using a preference system.
+					// -- yin
+					if (collectionType.equals(java.util.List.class))
+						collectionType = ArrayList.class;
+					
 					collection = collectionType.newInstance();
 					// set the field to the new collection
 					field.set(activeES, collection);
